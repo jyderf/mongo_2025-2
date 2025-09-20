1632ba8f7c0aeb0b2341502eb82988fb8e53f31a
jyderf <fredy.ingenieria@gmail.com>
2025-09-20 06:59:54 -0500

    se agregó un nuevo script uniendo 2 colecciones y ademas un condicional

---
+++ bd_ventas/3.trayendo_2_colecciones/productos_y_categorias/4.$cond_if_PRODUCTOS_cuya_CATEGORIA_sea_un_alimento_o_no.js
@@ -0,0 +1,42 @@
+use("supermarket");
+
+db.productos.aggregate([
+  {
+    $lookup: {
+      from: "categorias",
+      localField: "categoria_id",
+      foreignField: "_id",
+      as: "cat"
+    }
+  },
+  {
+    // Aplanamos el array 'cat' para poder acceder a sus campos directamente
+    // y usar el $match de forma efectiva.
+    $unwind: "$cat"
+  },
+  {
+    // El $match debe ir aquí, después de que 'cat' esté disponible
+    // y antes del $project que podría ocultarlo.
+    $match: {
+      $or: [{ "cat.nombre": "Frutas" }, { "cat.nombre": "Aseo" }]
+    }
+  },
+  {
+    // Finalmente, proyectamos los campos que queremos en el resultado.
+    $project: {
+      _id: 1,
+      nombre: 1,
+      categoria: "$cat.nombre", // Renombramos 'cat.nombre' a 'categoria'
+      precio: 1,
+      stock: 1,
+      proveedor: 1,
+      sub_categoria:{
+        $cond:{
+            if:{$eq:["$cat.nombre","Aseo"]},
+            then:"El producto no es un alimento",
+            else:"El producto es un alimento",
+        }
+      }
+    }
+  }
+]);
