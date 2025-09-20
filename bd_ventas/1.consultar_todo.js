a7050099854ea8d3a61d39a4afd6714b051dae89
jyderf <fredy.ingenieria@gmail.com>
2025-09-20 02:11:44 -0500

    primera_subida

---
+++ bd_ventas/1.consultar_todo.js
@@ -0,0 +1,3 @@
+use ("supermarket");
+
+db.ventas.aggregate([]);
---
+++ bd_ventas/4.con_$match_consulta_donde_id_se_igual_a_usando_$expr_y$eq.js
@@ -0,0 +1,21 @@
+use("supermarket");
+
+db.ventas.aggregate([
+
+    {
+        $project: {
+            "_id": 1,
+            "fecha": 1,
+            "items": 1,
+            "total": 1,
+        }
+    },
+    {
+        $match: {
+            $expr:{
+               $eq:["$_id",2]
+            }
+        }
+    }
+
+]);
