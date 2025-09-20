69f16ecf991048298acd686e43b128839cafd650
jyderf <fredy.ingenieria@gmail.com>
2025-09-20 05:44:02 -0500

    actualización ventas y productos

---
+++ bd_ventas/3.trayendo_2_colecciones/VENTAS_Y_PRODUCTOS_INCONCLUSO/5.Trer_campos_de_PRODUCTOS_relcionados_en_VENTAS.js
@@ -0,0 +1,30 @@
+use("supermarket");
+
+db.ventas.aggregate([ 
+    {
+        $lookup: {
+            from: "productos",
+            localField: "items.producto_id",
+            foreignField: "_id",
+            as: "prod"
+        }
+    }, 
+    
+    {
+        $match: {
+           $or:[{_id:{$eq:1}},{_id:{$eq:2}}]
+        }
+    },   
+    
+    {
+        $project: {
+                   _id: 1,
+                   cliente_id:1,
+                   fecha :1,   
+                   prod.nombre: 1,
+                   prod.precio: 1,
+                   total: 1,
+          } 
+       },  
+
+]);
