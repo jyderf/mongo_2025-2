f9a4212ae45c45c6a6930fd59f3e0316a4b7f8a9
jyderf <fredy.ingenieria@gmail.com>
2025-09-20 06:46:43 -0500

    reduciendo muchos más comentarios en productos y categorias

--- bd_ventas/3.trayendo_2_colecciones/productos_y_categorias/1.Todos_los_PRODUTOS_y_CATEGORIAS_donde_nombre_de_su_categoria_corresponda_al_producto.js
+++ bd_ventas/3.trayendo_2_colecciones/productos_y_categorias/1.Todos_los_PRODUTOS_y_CATEGORIAS_donde_nombre_de_su_categoria_corresponda_al_producto.js
@@ -10,17 +10,15 @@ db.productos.aggregate([
       as: "cat"                    // Nombre del nuevo campo que contendrá los documentos de la categoría (será un array)
     }
   },
-     /*Etapa 2: $unwindEsta etapa "desanida" el array 'cat' que se creó en la etapa anterior.Es útil para procesar cada elemento del array como un documento independiente,facilitando el acceso directo a sus campos en las etapas siguientes. */
-  {
+     
+  {/*Etapa 2: $unwindEsta etapa "desanida" el array 'cat' que se creó en la etapa anterior.Es útil para procesar cada elemento del array como un documento independiente,facilitando el acceso directo a sus campos en las etapas siguientes. */
     $unwind: "$cat"
   },
-  // Etapa 3: $project // Esta etapa define la forma final del documento de salida. // Permite seleccionar, renombrar y excluir campos.
-  {
+  {// Etapa 3: $project // Esta etapa define la forma final del documento de salida. // Permite seleccionar, renombrar y excluir campos.
     $project: {
       _id: 0,                      // Oculta el campo _id para un resultado más limpio
       nombre: 1,                   // Incluye el campo 'nombre' del producto
-      // Renombra el campo "cat.nombre" a "categoria" para la salida,
-      // creando un nuevo campo de nivel superior.
+      // creando un nuevo campo de nivel superior. // Renombra el campo "cat.nombre" a "categoria" para la salida,      
       categoria: "$cat.nombre", 
       precio: 1,                   // Incluye el campo 'precio'
       stock: 1,                    // Incluye el campo 'stock'
